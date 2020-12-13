f29072f10a925907e4e0299e5423f1fc1011329a
Arthur Barbosa <arthurbruno03@gmail.com>
2020-12-13 10:47:00 -0400

    Project hr-payroll created

---
+++ hr-payroll/src/main/java/com/abcode/hrpayroll/HrWorker1Application.java
@@ -0,0 +1,13 @@
+package com.abcode.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorker1Application.class, args);
+	}
+
+}
---
+++ hr-payroll/src/test/java/com/abcode/hrpayroll/HrWorker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcode.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
