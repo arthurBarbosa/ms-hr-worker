22bc4cd30837c80f40b214459a073ccc4f4f9577
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-27 15:45:20 -0400

    Zull authorization

--- hr-api-gateway-zuul/src/main/java/com/abcode/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/abcode/hrapigatewayzuul/config/AppConfig.java
@@ -1,18 +1,12 @@
-package com.abcode.hroauth.config;
+package com.abcode.hrapigatewayzuul.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
-	
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 	
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
@@ -25,6 +19,5 @@ public class AppConfig {
 	public JwtTokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
 	}
-	
 
 }
